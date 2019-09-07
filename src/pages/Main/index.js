89d32bae982c53f602ee1bd527705280ab828af9
Thalysson <thalyssonwg.nascimento@gmail.com>
2019-09-07 20:23:56 -0300

    incluindo a dependênicia react-icons

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,7 +1,28 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { FaGitAlt, FaPlus } from 'react-icons/fa';
+
+import { Container, Form, SubmitButton } from './styles';
 
 export default function Main() {
-    return <h1>Main</h1>;
+    return (
+        <Container>
+            <h1>
+                <FaGitAlt />
+                Repositório
+            </h1>
+
+            <Form onSubmit={() => {}}>
+                <input
+                    type="text"
+                    placeholder="Adicionar repositório"
+                    name=""
+                    id=""
+                />
+                <SubmitButton>
+                    <FaPlus color="#fff" size={14} />
+                </SubmitButton>
+            </Form>
+        </Container>
+    );
 }
